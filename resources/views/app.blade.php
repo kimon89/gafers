b177fe069592b049704ff15414e2cbc510a832b3
Kimon Moutsakis <moutsakisk@gmail.com>
2015-07-24 10:29:25 +0100

    facebook app id

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -53,7 +53,7 @@
             var js, fjs = d.getElementsByTagName(s)[0];
             if (d.getElementById(id)) return;
             js = d.createElement(s); js.id = id;
-            js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1&appId=651453621654315";
+            js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1&appId=<?=config('app.facebook_app_id')?>";
             fjs.parentNode.insertBefore(js, fjs);
           }(document, 'script', 'facebook-jssdk'));
         </script>
