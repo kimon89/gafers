f65fc2ec548e3435445939f9018f98f1db4f9910
Kimon Moutsakis <moutsakisk@gmail.com>
2015-07-25 15:24:14 +0100

    default avatar fix

--- app/User.php
+++ app/User.php
@@ -23,7 +23,7 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
 	 *
 	 * @var array
 	 */
-	protected $fillable = ['username', 'email', 'password','activation_code','active','facebook_id'];
+	protected $fillable = ['username', 'email', 'password','activation_code','active','facebook_id','default_avatar'];
 
 	/**
 	 * The attributes excluded from the model's JSON form.
